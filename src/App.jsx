54a5ab13e1736c50603839c18b6cb8e2727aaa7d
Matthew Stinis <mpstinis@gmail.com>
2024-07-31 18:10:01 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Calendar from "./pages/Calendar/Calendar";
+
+function App() {
+  return (
+    <div>
+      <h1>Event Horizon - Montreal</h1>
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
