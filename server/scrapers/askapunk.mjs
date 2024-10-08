701e0e8d0972ea664d370597060d44edbe8b0515
Matthew Stinis <mpstinis@gmail.com>
2024-10-07 21:35:54 -0400

    finish askapunk data

--- server/scrapers/askapunk.mjs
+++ server/scrapers/askapunk.mjs
@@ -1,13 +1,48 @@
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc.js';
+import timezone from 'dayjs/plugin/timezone.js';
 import { getUnformattedEvents } from "./helpers.mjs";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 const getAskAPunk = async () => {
     const unformattedEvents = await getUnformattedEvents("https://montreal.askapunk.net/api/events");
     
-    unformattedEvents.map(event => {
-        //
-        console.log(event);
-        
-    })
+    const formattedEvents = unformattedEvents.map(event => {
+       const {
+        id,
+        title,
+        slug,
+        start_datetime,
+        media,
+        place
+      } = event;
+      
+      const rawDateShowTime = dayjs.unix(start_datetime)
+      const dateShowTime = dayjs.utc(rawDateShowTime).tz('America/New_York').toISOString();
+
+      const image = media ? media[0].url : null;
+      const venue = place ? place.name : null;
+      const address = place ? place.address : null;
+      const cleanTitle = title.replace(/[^ -~]+/g, "/");
+      
+      return {
+        originalId: id,
+        title: cleanTitle,
+        dateShowTime,
+        dateDoorTime: null,
+        venue,
+        address,
+        price: null,
+        image,
+        ticketLink: null,
+        moreInfoLink: `https://montreal.askapunk.net/events/${slug}`,
+        source: "askapunk"
+      }
+    });
+
+    return formattedEvents;
 }
 
-getAskAPunk();
+export { getAskAPunk };
